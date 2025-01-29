f67fe67a4e21c75020f3f4af1f0679d811863f4f
winnergeoff <winner.geoff@gmail.com>
2025-01-29 13:00:21 -0800

    Update copy for statsring component

--- src/components/StatsRing/StatsRing.tsx
+++ src/components/StatsRing/StatsRing.tsx
@@ -8,10 +8,10 @@ const icons = {
 };
 
 const data = [
-  { label: 'Page views', stats: '456,578', progress: 65, color: 'teal', icon: 'up' },
-  { label: 'New users', stats: '2,550', progress: 72, color: 'blue', icon: 'up' },
+  { label: 'Words Written', stats: '456,578', progress: 65, color: 'teal', icon: 'up' },
+  { label: 'Active Writers', stats: '2,550', progress: 72, color: 'blue', icon: 'up' },
   {
-    label: 'Orders',
+    label: 'Hours Saved',
     stats: '4,735',
     progress: 52,
     color: 'red',
